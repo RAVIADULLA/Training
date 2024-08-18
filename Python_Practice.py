a02d9468e4437be36c4a54f6d87244f8d09ea2de
Ravi Adulla <ravi.adulla2@gmail.com>
2024-08-17 20:03:42 -0500

    testing git

---
+++ Python_Practice.py
@@ -0,0 +1,8 @@
+empid = 1234
+name = "Ravi"
+age=38
+qual="btech"
+print(empid,"hi i am",name, "my age is", age,"i have completed my",qual)
+print("{0} hi i am {1} my age is {2} i have completed my {3}".format(empid,name,age,qual))
+print("{0} hi i am {1} my age is {2} i have completed my {3}".format(name,empid,age,qual))
+print("{e} hi i am {n} my age is {a} i have completed my {q}".format(e=empid,n=name,a=age,q=qual))
